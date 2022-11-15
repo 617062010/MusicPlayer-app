a385b2bf46afea2ca0794ecb8280b4b8ee4278ee
617062010 <73211829+617062010@users.noreply.github.com>
2022-11-15 14:06:23 +0800

    Add files via upload

---
+++ src/plugins/index.js
@@ -0,0 +1,15 @@
+import { Button, Swipe, SwipeItem, Popup } from 'vant';
+
+
+
+
+let plugins = [
+    Button,Swipe,SwipeItem, Popup
+]
+
+
+export default function getVant(app){
+    plugins.forEach((item) => {
+        return app.use(item)
+    })
+}
